f3dcde7575da8ec55cf0af3ab77fcdf325cf3cb9
silvakwan1 <154025472+dieggo25silva@users.noreply.github.com>
2024-03-31 02:37:27 -0300

    ajustes no css e html

--- src/count.js
+++ src/count.js
@@ -1,17 +1,15 @@
+function updateCounter() {
+  const countElement = document.querySelector(".count-text");
 
-function updateCounter () {
-    const countElement = document.querySelector(".count-text")
-    
-    let count = Number(countElement.textContent)
-    
-   
-    if(count < 38){
-        count+= 0.1
-    }else{
-        clearInterval(interval)
-    }
-   
-    countElement.textContent = count.toFixed(2)
+  let count = Number(countElement.textContent);
+
+  if (count < 38.59) {
+    count += 0.1;
+  } else {
+    clearInterval(interval);
   }
-  
-const interval = setInterval(updateCounter,100)
+
+  countElement.textContent = count.toFixed(2);
+}
+
+const interval = setInterval(updateCounter, 100);
